f76b95396f85793623b3f66e6ace676fcb51b590
RiseStudioOfficial <rise.studio.company@gmail.com>
2025-08-10 15:29:35 +0300

    Update video_maker.py

--- video_maker.py
+++ video_maker.py
@@ -23,8 +23,7 @@ def create_video(quote, background_url, bg_type):
         quote,
         fontsize=50,
         color='white',
-        font='Arial-Bold',
-        method='label',
+        method='caption',
         size=(clip.w * 0.8, None)
     ).set_position('center').set_duration(10)
 
