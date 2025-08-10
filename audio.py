aa685f4dd9d975da13967c36c25385e1a7eb77cb
RiseStudioOfficial <rise.studio.company@gmail.com>
2025-08-10 19:22:35 +0300

    Create audio.py

---
+++ audio.py
@@ -0,0 +1,24 @@
+from google.cloud import texttospeech
+
+def synthesize_speech(text, output_filename="speech.mp3"):
+    client = texttospeech.TextToSpeechClient()
+
+    input_text = texttospeech.SynthesisInput(text=text)
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="ru-RU",
+        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        input=input_text, voice=voice, audio_config=audio_config
+    )
+
+    with open(output_filename, "wb") as out:
+        out.write(response.audio_content)
+
+    return output_filename
