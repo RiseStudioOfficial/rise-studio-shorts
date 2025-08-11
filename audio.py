35cacc9b8fbfc8e740d0d4ca92cae3503826e15c
RiseStudioOfficial <rise.studio.company@gmail.com>
2025-08-11 11:57:52 +0300

    Update audio.py

--- audio.py
+++ audio.py
@@ -3,7 +3,8 @@ import io
 
 def synthesize_speech(text, lang="ru"):
     tts = gTTS(text=text, lang=lang)
-    fp = io.BytesIO()
-    tts.write_to_fp(fp)
-    fp.seek(0)
-    return fp
+    # Создаем временный файл с нужным суффиксом
+    tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix="speech.mp3")
+    tmp_file.close()  # Закрываем файл, чтобы gTTS мог записать
+    tts.save(tmp_file.name)  # Сохраняем аудио в файл
+    return tmp_file.name  # Возвращаем путь к файлу
