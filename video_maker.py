77f1fa32c4d39232a227d33f5df0ad5157c31c97
RiseStudioOfficial <rise.studio.company@gmail.com>
2025-08-11 11:37:11 +0300

    Update video_maker.py

--- video_maker.py
+++ video_maker.py
@@ -17,14 +17,22 @@ def create_vertical_video(quote, background_url, bg_type):
     bg_file = "background.mp4" if bg_type == "video" else "background.jpg"
     download_file(background_url, bg_file)
 
-    if bg_type == "image":
-        clip = ImageClip(bg_file).with_duration(10).resized((1080, 1920))
-    else:
-        # clip = VideoFileClip(bg_file).subclipped(0, 10).resized(height=1920)
-        # x_center = clip.w / 2
-        # clip = crop(clip, x_center=x_center, width=1080)
-        clip = clip.resize(height=1920)
+    if bg_type == "video":
+        clip = VideoFileClip(background_url)
         clip = Crop(x_center=clip.w / 2, width=1080, height=1920, y_center=clip.h / 2)(clip)
+    elif bg_type == "image":
+        clip = ImageClip(bg_file).with_duration(10).resized((1080, 1920))  # 10 секунд или сколько надо
+    else:
+        raise ValueError(f"Неизвестный тип: {bg_type}")
+    
+    # if bg_type == "image":
+    #     clip = ImageClip(bg_file).with_duration(10).resized((1080, 1920))
+    # else:
+    #     # clip = VideoFileClip(bg_file).subclipped(0, 10).resized(height=1920)
+    #     # x_center = clip.w / 2
+    #     # clip = crop(clip, x_center=x_center, width=1080)
+    #     clip = clip.resize(height=1920)
+    #     clip = Crop(x_center=clip.w / 2, width=1080, height=1920, y_center=clip.h / 2)(clip)
 
     txt_clip = TextClip(
         text=quote,
