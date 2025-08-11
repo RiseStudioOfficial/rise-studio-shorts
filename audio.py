32b6f1624ff8fcd4b9b0af1ed5d0b804187fb88e
RiseStudioOfficial <rise.studio.company@gmail.com>
2025-08-11 11:31:59 +0300

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,24 +1,9 @@
-from google.cloud import texttospeech
+from gtts import gTTS
+import io
 
-def synthesize_speech(text, output_filename="speech.mp3"):
-    client = texttospeech.TextToSpeechClient()
-
-    input_text = texttospeech.SynthesisInput(text=text)
-
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="ru-RU",
-        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
-    )
-
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3
-    )
-
-    response = client.synthesize_speech(
-        input=input_text, voice=voice, audio_config=audio_config
-    )
-
-    with open(output_filename, "wb") as out:
-        out.write(response.audio_content)
-
-    return output_filename
+def synthesize_speech(text, lang="ru"):
+    tts = gTTS(text=text, lang=lang)
+    fp = io.BytesIO()
+    tts.write_to_fp(fp)
+    fp.seek(0)
+    return fp
